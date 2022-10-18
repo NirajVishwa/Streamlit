76ac811db21f04d9797e0c613c81f8f2da5ea4d2
NirajVishwa <114505992+NirajVishwa@users.noreply.github.com>
2022-10-18 21:44:22 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+@st.cache
+def get_un_data():
+    df=pd.read_csv("D:\\data\\agri.csv")
+    return df.set_index("Region")
+df=get_un_data()
+countries=st.multiselect("Choose countries",list(df.index),default=["United States of America","India"])
+data=df.loc[countries]
+data/=1000000.0
+st.write("### gross agriculture production ($B)",data.sort_index())
+data=data.T.reset_index()
+data=pd.melt(data,id_vars=["index"]).rename(
+    columns={"index":"year","value":"gross agriculture product ($B)"}
+)
+chart=alt.Chart(data).mark_area(opacity=0.3).encode(x="year:T",y=alt.Y("gross agriculture product ($B):Q", stack=None),color="Region:N")
+st.altair_chart(chart,use_container_width=True)
